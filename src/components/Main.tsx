58201b6a3d8d2d43bb18bd193821cbe8510664de
farhan <farhansyed11.3@gmail.com>
2025-12-20 16:16:18 -0800

    centered hero on page

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,7 +6,7 @@ function Layout() {
   return (
     <div className="min-h-screen flex flex-col bg-zinc-950 text-zinc-100">
       <Header />
-      <main className="flex-1">
+      <main className="flex-1 flex flex-col justify-center">
         {/*<div className="w-full max-h-full overflow-auto">*/}
         <Outlet />
         {/*</div>*/}
